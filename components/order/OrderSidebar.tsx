3cec54242993f53d5be9814ff6807f591b6a297e
emiliorz2 <emiliorz2@icloud.com>
2024-09-12 14:27:44 -0600

    Refactor code and add order functionality

--- components/order/OrderSidebar.tsx
+++ components/order/OrderSidebar.tsx
@@ -1,10 +1,28 @@
 
+import { prisma } from '@/src/lib/prisma'
+import CategoryIcon from '../ui/CategoryIcon'
 
-import React from 'react'
 
-export default function OrderSidebar() {
+
+
+async function getCategories() {
+  const categories = await prisma.category.findMany()
+  return categories
+}
+
+export default async function OrderSidebar() {
+
+  const categories = await getCategories()
+  
+  
   return (
     <aside className='md:w-72 md:h-screen bg-white'>
+
+        <nav className='mt-10'>
+          {categories.map((category) =>  (
+            <CategoryIcon key={category.id} category={category} />
+          ))}
+        </nav>
 
     </aside>
   )
