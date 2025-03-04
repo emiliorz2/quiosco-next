334905d6e06bfac38f57ac3d7655e4961b0fbaa6
emiliorz2 <emiliorz2@icloud.com>
2025-03-03 21:22:21 -0600

    Update heading text in CreateProductPage for clarity

--- app/admin/products/new/page.tsx
+++ app/admin/products/new/page.tsx
@@ -8,7 +8,7 @@ import React from 'react'
 export default function CreateProductPage() {
   return (
     <>
-      <Heading>Nuevo producto</Heading>
+      <Heading>Agregar Nuevo producto</Heading>
 
       <AddProductForm >
         <ProductForm />
